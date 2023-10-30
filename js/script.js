f1cd74fa77aa24937bf1db5962d2009637b2df6a
chiara1911 <chiaracesari89@gmail.com>
2023-10-30 18:27:57 +0100

    fix bonus

--- js/script.js
+++ js/script.js
@@ -24,65 +24,64 @@
 
 // console.log(slides);
 
-const {createApp} = Vue
+const { createApp } = Vue;
 
-createApp ({
-    data(){
-        return {
-           slides : [
-                {
-                    image: 'img/01.webp',
-                    title: 'Marvel\'s Spiderman Miles Morale',
-                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                }, 
-                {
-                    image: 'img/02.webp',
-                    title: 'Ratchet & Clank: Rift Apart',
-                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                }, 
-                {
-                    image: 'img/03.webp',
-                    title: 'Fortnite',
-                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                }, 
-                {
-                    image: 'img/04.webp',
-                    title: 'Stray',
-                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                }, 
-                {
-                    image: 'img/05.webp',
-                    title: "Marvel's Avengers",
-                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                }
-                ],
-                activeSlides : 0
-        }
-    },
-    methods: {        
-       
-
-        prev(){
-            if(this.activeSlides === 0){
-                this.activeSlides = this.slides.length -1;
-            } else{
-                this.activeSlides --;
-            }
+createApp({
+  data() {
+    return {
+      slides: [
+        {
+          image: "img/01.webp",
+          title: "Marvel's Spiderman Miles Morale",
+          text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
         },
-
-        next(){
-            if(this.activeSlides === this.slides.length -1){
-            this.activeSlides = 0;
-        } else{
-            this.activeSlides ++;
-        }
+        {
+          image: "img/02.webp",
+          title: "Ratchet & Clank: Rift Apart",
+          text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        },
+        {
+          image: "img/03.webp",
+          title: "Fortnite",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        },
+        {
+          image: "img/04.webp",
+          title: "Stray",
+          text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        },
+        {
+          image: "img/05.webp",
+          title: "Marvel's Avengers",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        },
+      ],
+      activeSlides: 0,
+    };
+  },
+  methods: {
+    prev() {
+      if (this.activeSlides === 0) {
+        this.activeSlides = this.slides.length - 1;
+      } else {
+        this.activeSlides--;
+      }
     },
 
-    getActive(index){
-  if(this.activeSlides === index){
-this.classList.add('active')
-  }
+    next() {
+      if (this.activeSlides === this.slides.length - 1) {
+        this.activeSlides = 0;
+      } else {
+        this.activeSlides++;
+      }
+    },
+
+    getActive(index) {
+        return index === this.activeSlides ? 'active' : '';
+     },
+    activeImg(index){
+        this.activeSlides = index
     }
-}
-    
-}).mount('#app')
+
+  },
+}).mount("#app");
